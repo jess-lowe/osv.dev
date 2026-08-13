0c9ab62500b4e7f4c398b716607173d3701c04fb
Jess Lowe <jesslowe@google.com>
2026-08-13 04:44:49 +0000

    Refactor to be strategy based

--- vulnfeeds/conversion/cve5/extraction.go
+++ vulnfeeds/conversion/cve5/extraction.go
@@ -1,6 +1,8 @@
 package cve5
 
 import (
+	"strings"
+
 	"github.com/google/osv/vulnfeeds/models"
 	"github.com/google/osv/vulnfeeds/vulns"
 )
@@ -11,18 +13,47 @@ type VersionExtractor interface {
 	FindNormalAffectedRanges(affected models.Affected, metrics *models.ConversionMetrics) ([]models.RangeWithMetadata, VersionRangeType)
 }
 
-// GetVersionExtractor returns the appropriate VersionExtractor for a given CNA.
+// GetVersionExtractor returns the appropriate VersionExtractor configured with CNA-specific strategies.
 func GetVersionExtractor(cna string) VersionExtractor {
-	switch cna {
-	case "Linux":
-		return &LinuxVersionExtractor{}
-	case "Wordfence":
-		return &WordpressExtractor{Handler: &WordfenceHandler{}}
-	case "Patchstack":
-		return &WordpressExtractor{Handler: &PatchstackHandler{}}
-	case "WPScan":
-		return &WordpressExtractor{Handler: &WPScanHandler{}}
+	switch strings.ToLower(cna) {
+	case "linux":
+		return &LinuxVersionExtractor{
+			DefaultVersionExtractor: DefaultVersionExtractor{
+				Strategies: LinuxStrategies(),
+			},
+		}
+	case "wordfence":
+		return &WordpressExtractor{
+			Handler: &WordfenceHandler{},
+			DefaultVersionExtractor: DefaultVersionExtractor{
+				Strategies: WordfenceStrategies(),
+			},
+		}
+	case "patchstack":
+		return &WordpressExtractor{
+			Handler: &PatchstackHandler{},
+			DefaultVersionExtractor: DefaultVersionExtractor{
+				Strategies: PatchstackStrategies(),
+			},
+		}
+	case "wpscan":
+		return &WordpressExtractor{
+			Handler: &WPScanHandler{},
+			DefaultVersionExtractor: DefaultVersionExtractor{
+				Strategies: WPScanStrategies(),
+			},
+		}
+	case "github_m", "github":
+		return &DefaultVersionExtractor{
+			Strategies: GitHubStrategies(),
+		}
+	case "mitre":
+		return &DefaultVersionExtractor{
+			Strategies: MITREStrategies(),
+		}
 	default:
-		return &DefaultVersionExtractor{}
+		return &DefaultVersionExtractor{
+			Strategies: DefaultStrategies(),
+		}
 	}
 }
